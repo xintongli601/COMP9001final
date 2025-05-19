29cfbc35d9a7cdd82f4e27e25812a8362e63b048
xintongli601 <xintongli601@gmail.com>
2025-05-19 23:04:03 +1000

    Create main.py

---
+++ BlackJack/main.py
@@ -0,0 +1,9 @@
+from game import BlackjackGame
+
+if __name__ == '__main__':
+    """
+    This block starts the Blackjack game. It creates an instance of the BlackjackGame class 
+    and calls the play() method to launch the game.
+    """
+    game = BlackjackGame()  # Create a new game instance
+    game.play()             # Start the game loop
